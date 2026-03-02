4537e14913941e3e98e563574e97bb459a89dc0b
chloeyloh <254973343+chloeyloh@users.noreply.github.com>
2026-03-02 12:22:10 -0500

    Updates

--- script.js
+++ script.js
@@ -3,11 +3,25 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiY2hsb2Vsb2giLCJhIjoiY21sZ3UwZWYzMDIyazNocHNmc
 
 const map = new mapboxgl.Map({
     container: 'my-map', // ID of the HTML element to contain the map
-    style: 'mapbox://styles/chloeloh/cmlguusrw00jx01qq87ehggny', // Map style URL
+    style: 'mapbox://styles/chloeloh/cmm9ci4gs001301ry3yjqgd6x', // Map style URL
     center: [-79.3832, 43.6532], // Initial map starting position
     zoom: 12 // Initial zoom position
 });
 
+// Sets map load event listener to add the GeoJSON data source and layer
+map.on('load', () => {
+    // Adding my Mapbox style as a vector source to the map
+    map.addSource('the-beach-pointsofinterest', {
+        type: 'vector',
+        url: 'mapbox://styles/chloeloh/cmm9ci4gs001301ry3yjqgd6x' // Map style URL containing GeoJSON data source
+    });
+
+    // Adding my GeoJSON for points of interest (coffee, parks, recreation)
+    map.addSource('points-of-interest', {
+        type: 'geojson',
+        data: 'https://github.com/chloeyloh/ggr472-lab3/blob/main/lab3map.geojson' // Raw link to GeoJSON file in GitHub repository
+
 // Adding navigation controls to the map
 map.addControl(new mapboxgl.NavigationControl(), 'top-right');
+
 
