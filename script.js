b80a20509e4fab64ade9acc1f9c7f4febe8b2eb6
chloeyloh <254973343+chloeyloh@users.noreply.github.com>
2026-03-02 12:41:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiY2hsb2Vsb2giLCJhIjoiY21sZ3UwZWYzMDIyazNocHNmc
 const map = new mapboxgl.Map({
     container: 'my-map', // ID of the HTML element to contain the map
     style: 'mapbox://styles/chloeloh/cmm9ci4gs001301ry3yjqgd6x', // Map style URL
-    center: [-79.3832, 43.6532], // Initial starting position in downtown Toronto
-    zoom: 12 // Initial zoom position
+    center: [-79.2980, 43.6690], // Initial starting position on the Beach BIA
+    zoom: 14 // Initial zoom position
 });
 
 // Sets map load event listener to add the GeoJSON data source and layer
